1bf0d373ce45bc71cbd253a7ff432324303f4728
Shwetaank <spmorey87@gmail.com>
2024-12-30 18:47:05 +0530

    small update

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  images: {
+    loader: "custom",
+    loaderFile: "./loader.js",
+  },
   /* config options here */
 };
 
